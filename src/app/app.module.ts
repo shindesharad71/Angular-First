5565957efed6d5113b6adc22ad531a96800faf7f
Sharad Shinde <shindesharad71@gmail.com>
2017-09-02 16:37:39 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ShoppingComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
