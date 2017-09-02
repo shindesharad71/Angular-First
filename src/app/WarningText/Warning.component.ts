b5015d4c06a91bb07eeb9979dd178e015a0797b6
Sharad Shinde <shindesharad71@gmail.com>
2017-09-02 15:22:50 +0530

    updated

---
+++ src/app/WarningText/Warning.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './Warning.component.html',
+  styleUrls: ['./Warning.component.css']
+})
+
+export class WarningComponent {
+
+}
