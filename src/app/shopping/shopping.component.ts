e62e8b54961eb7deb34c6e62f25f1a8f71573dc1
Sharad Shinde <shindesharad71@gmail.com>
2017-09-03 03:58:51 +0530

    ingredients section added

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 12),
+    new Ingredient('Tomatoes', 2)
+  ];
 
   constructor() { }
 
