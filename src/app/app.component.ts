56a8c235073b744df67e80266fe2aec34633e4e6
Sharad Shinde <shindesharad71@gmail.com>
2017-09-14 23:52:53 +0530

    firebase config setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD553RjwNlc5L3P3spxgmq129MG5MZIiU4",
+      authDomain: "angular-http-25969.firebaseapp.com"
+    });
+  }
+
 }
