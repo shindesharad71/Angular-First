58ce313c769030c77279c18d3c56415250639330
Sharad Shinde <shindesharad71@gmail.com>
2017-09-15 17:29:47 +0530

    token auth added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,19 +3,23 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 import { RecipeService } from '../recipe/recipe.service';
 import { Recipe } from '../recipe/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 
 export class DataStorageService {
 
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http, private recipeService: RecipeService,
+  private authService: AuthService) {}
 
   storeRecipes() {
-    return this.http.put('https://angular-http-25969.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put('https://angular-http-25969.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://angular-http-25969.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+    this.http.get('https://angular-http-25969.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
