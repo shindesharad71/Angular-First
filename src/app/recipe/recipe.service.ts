6bfb7068355d1ba37f51a15ae1189740b7d2dea8
Sharad Shinde <shindesharad71@gmail.com>
2017-09-19 15:27:36 +0530

    default recipes updated

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -9,13 +9,17 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is Simply a test', 'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg', [
-      new Ingredient('Apples', 12),
-      new Ingredient('Tomatoes', 2)
+    new Recipe('Vada Paav',
+    'Vada pav, sometimes spelled Wada Pav or Vada Pav or Wada Pao, is a vegetarian fast food dish native to the Indian state of Maharashtra. The dish is a simple creation involving a deep fried potato patty with some coriander and spices, served in a bread roll (pav) with condiments. Usually people eat it with chatni and onions It originated as cheap street food in Mumbai, but is now offered in stalls and restaurants throughout India.',
+    'http://www.dfordelhi.in/wp-content/uploads/2016/05/vada-pav-7.jpg', [
+      new Ingredient('Potato', 2),
+      new Ingredient('Bread', 1)
     ]),
-    new Recipe('A Test Recipe 2', 'This is Simply a test 2', 'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg', [
-      new Ingredient('Apples', 12),
-      new Ingredient('Tomatoes', 2)
+    new Recipe('Dosa',
+    'Dosa is a type of pancake from the Indian subcontinent, made from a fermented batter. It is somewhat similar to a crepe in appearance. Its main ingredients are rice and black gram. Dosa is a typical part of the South Indian diet and popular all over the Indian subcontinent. Traditionally, Dosa is served hot along with sambar, stuffing of potatoes or paneer and chutney. It can be consumed with idli podi as well.',
+    'http://www.ndtv.com/cooks/images/mysore.masala.dosa.1%20%281%29.jpg', [
+      new Ingredient('Rice', 12),
+      new Ingredient('Potatoes', 2)
     ])
   ];
 
